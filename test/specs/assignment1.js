e2936636371710aa54a75205e8d5a22a38cddd13
Paul <paul.dadarlat@gmail.com>
2021-11-11 15:47:53 +0200

    commit

--- test/specs/assignment1.js
+++ test/specs/assignment1.js
@@ -2,18 +2,14 @@ describe('My application', () => {
     it('results should match the search criteria', async () => {
         await browser.url('https://www.bayut.com/');
         await expect(browser).toHaveTitle('Bayut: UAE\'s Largest Real Estate Portal');
-
         await $('._5176f8e1=purpose').click();
         await $('[aria-label="Buy"]').click();
-
         const searchInput = await $('//*[@id="body-wrapper"]/header/div[4]/div/div[2]/div/div[1]/div[2]/div/div/ul/input');
         await searchInput.setValue('dubai marina');
         await browser.pause(200);
-
         await browser.keys("Enter");
         await $('//*[@id="body-wrapper"]/header/div[4]/div/div[2]/div/div[2]/a').click();
         await browser.pause(5000);
-
         await expect(browser).toHaveTitle("Properties for Sale in Dubai Marina | Bayut.com");
         const articles = await $$('[aria-label = Location]');
         articles.forEach(async element  => {
--- test/specs/assignment2v2.js
+++ test/specs/assignment2v2.js
@@ -1,19 +1,18 @@
-const fetch = require("node-fetch")
 describe('My application', () => {
-    it('should check if all the links are working correctly', async () => {
+    it('should check if all the search pages are loaded correctly', async () => {
         await browser.url('https://www.bayut.com/');
         await $('//*[@id="body-wrapper"]/main/div[6]/div/div[1]').scrollIntoView();
         await $('//*[@id="body-wrapper"]/main/div[6]/div/div[2]/div[2]/div/div/div[2]').click();
         const linksParent = await $('//*[@id="body-wrapper"]/main/div[6]/div/div[2]/div[1]/div[2]/div/div[1]/div[1]/div/div/div[2]/div[1]/ul');
         const links = await linksParent.$$('a');
         const urlPromises = await links.map(link => link.getAttribute('href'))
+        const titlePromises = await links.map(title => title.getAttribute('title'))
+        const titles = await Promise.all(titlePromises)
         const urls = await Promise.all(urlPromises)
-        const requests = urls.map(url => fetch("https://www.bayut.com" + url));
-        const responses = await Promise.all(requests);
-        const statusCodes = responses.map(response => response.status);
-        statusCodes.forEach(statusCode => {
-            expect(statusCode).toEqual(200);
-        })
+        for (let i=0;i<urls.length;i++){
+            await browser.url("https://www.bayut.com" + urls[i]);
+            await expect(browser).toHaveTitleContaining(titles[i]).ignoreCase;
+        }
     });
 });
 
